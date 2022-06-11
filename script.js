9e1e8fac31d311f90cfcddf0f29cdd46c0e51d74
eb <eiler.byberi@gmail.com>
2022-06-10 21:54:51 -0700

    Add the option of adding or removing border

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ const shaderBtn = document.querySelector('.shader');
 const randomBtn = document.querySelector('.random');
 const eraseBtn = document.querySelector('.erase');
 const borderBtn = document.querySelector('.border');
+const backgroundBtn = document.querySelector('.background');
 
 let squareDimension = 720 / 16;
 let color = 'black';
 let gridSize = 16;
 
 document.addEventListener('DOMContentLoaded', grid)
+
+// function coloring(event) {
+//     //for (let i = 0; i < gridSize * gridSize; i++) {
+//         if (eraseBtn.getAttribute('class') === 'erasing') {
+//             squares[i].removeEventListener('mouseover', coloring);
+//             //continue;
+//             return;
+//         }
+//         squares[i].addEventListener('mouseover', () => {
+//             squares[i].style.backgroundColor = color;
+//         })
+//     //}
+// }
 
 function grid() {
     for (let i = 0; i < gridSize * gridSize; i++) {
@@ -20,9 +34,10 @@ function grid() {
         square.style.height = squareDimension + 'px';
         square.style.width = squareDimension + 'px';
         square.style.border = 'solid black thin';
+        square.style.backgroundColor = 'white';
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = color;
-        })
+        });
         container.appendChild(square);
     }
 }
@@ -46,20 +61,51 @@ randomBtn.textContent = 'RGB Mode';
 
 eraseBtn.textContent = 'Eraser';
 eraseBtn.addEventListener('click', () => {
+    eraseBtn.setAttribute('class', 'erasing');
     const squares = document.querySelectorAll('.square');
-    const squareArray = Array.fromArray(squares);
+    const squareArray = [...squares];
     squareArray.forEach(square => {
-        square.removeEventListener('mouseover', tr);
+        //square.removeEventListener('mouseover');
+        color = 'white';
         square.addEventListener('mousover', () => {
-            square.style.backgroundColor = 'white';
+            square.style.backgroundColor = color;
         } )
     });
 });
 
 borderBtn.textContent = 'Remove borders';
+borderBtn.setAttribute('class', 'borderyes');
+borderBtn.addEventListener('click', toggleBorders)
+
+backgroundBtn.textContent = 'Square color';
 
 function newGrid() {
     container.innerHTML = '';
     squareDimension = 720 / gridSize;
     grid();
+}
+
+function toggleBorders() {
+    const squares = document.querySelectorAll('.square');
+    const squaresArray = [...squares];
+
+    if (borderBtn.getAttribute('class') === 'borderyes') {
+        borderBtn.setAttribute('class', 'borderno');
+        borderBtn.textContent = 'Add borders';
+
+        squaresArray.forEach(square => {
+            square.style.border = 'none';
+        });
+    } else if (borderBtn.getAttribute('class') === 'borderno') {
+        borderBtn.setAttribute('class', 'borderyes');
+        borderBtn.textContent = 'Remove borders';
+
+        squaresArray.forEach(square => {
+            square.style.border = '1px solid black';
+        });
+    }
+
+    
+
+    
 }
